243d792f796b43579366657f4ce4af8ee20cd14c
Jon Barnes <103963671+barnes-jon@users.noreply.github.com>
2024-07-22 20:36:24 -0700

    made a larger site

---
+++ chatgpt code projects/larger_site/js/main.js
@@ -0,0 +1,13 @@
+document.getElementById('mainSection').innerHTML = `
+  <section>
+    <article>
+      <h2>Article Title</h2>
+      <p>Article content goes here...</p>
+    </article>
+    <article>
+      <h2>Another Title</h2>
+      <p>More content goes here...</p>
+    </article>
+  </section>
+`;
+ 
---
+++ chatgpt code projects/larger_site/js/topBanner.js
@@ -0,0 +1,1 @@
+document.getElementById('topBanner').innerHTML = `<header><h1>Welcome to My Website</h1></header>`;
